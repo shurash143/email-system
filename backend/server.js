de068471e53adb1b89d331e363aa25d17201986c
shurash143 <shukrigarad32@gmail.com>
2026-09-10 06:09:52 +0300

    email-system

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const FRONTEND_URL =
-  process.env.FRONTEND_URL || "http://localhost:5173";
+  process.env.FRONTEND_URL || "https://email-system-three.vercel.app/";
 
 // =========================================================
 // TEST ROUTE — REGISTER THIS FIRST
